0da603df2a78f1839f96157a7798f965dc6f7982
Ernesto Rojas <ernesto20145@gmail.com>
2018-09-03 16:10:44 -0300

    Added new role to get client by id services.

--- api/routers/clients.js
+++ api/routers/clients.js
@@ -4,10 +4,11 @@ const router = new Router({ prefix: '/clients' });
 const { ClientController } = cano.app.controllers;
 const { AuthPolices: { apiKey } } = cano.app.policies;
 const isApigateway = apiKey('apiGateway');
+const isApigatewayAndUser = apiKey(['apiGateway', 'user']);
 
 router.post('/', isApigateway, ClientController.create);
 router.get('/', isApigateway, ClientController.get);
-router.get('/:id', isApigateway, ClientController.getById);
+router.get('/:id', isApigatewayAndUser, ClientController.getById);
 router.get('/:id/contracts', isApigateway, ClientController.getContracts);
 router.put('/:id', isApigateway, ClientController.updateById);
 router.delete('/:id', isApigateway, ClientController.deleteById);
